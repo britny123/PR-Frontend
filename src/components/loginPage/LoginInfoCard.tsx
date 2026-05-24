fc018f4f8ebf9bfb0928d2663ce105af02e9b6d1
JohanMcBrain <johandrums09@gmail.com>
2026-05-24 17:33:15 -0600

    Se agregó el Login y el Register

---
+++ src/components/loginPage/LoginInfoCard.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import InputField from "../reusableComponents/InputField";
+import ButtonLarge from "../reusableComponents/LargeButton";
+
+export default function LoginInformationCard() {
+  //  Por mientras, luego se conecta con la base de datos y backend para guardar los datos
+    const [user, setUser] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSave = () => {
+    console.log("Saved login info:", {
+        user,
+        password,
+    });
+};
+
+return (
+    <div className="w-87.5 bg-white rounded-4xl border border-gray-300 flex flex-col gap-4 p-6 sm:p-10 justify-center items-center">
+        <h2 className="text-blue text-xl font-bold text-center title">
+            Login
+        </h2>
+
+        <InputField
+            placeholder="User"
+            type="text"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+        />
+
+        <InputField
+            placeholder="Password"
+            type="text"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <ButtonLarge onClick={handleSave} text="Save" />
+    </div>
+    );
+}
